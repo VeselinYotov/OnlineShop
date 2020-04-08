721fc880dec533a209a350ade22b4555992d1704
Veselin <veskotoyotov@gmail.com>
2020-04-08 22:03:33 +0300

    Updated ObjectRepository

--- Core/BaseObject/ObjectRepository.php
+++ Core/BaseObject/ObjectRepository.php
@@ -9,7 +9,7 @@ require_once('Core/DatabaseConnection.php');
  * 
  * Repository for Core\BaseObject\BaseObject
  */
-abstract class ObjectRepository implements ObjectInterface
+class ObjectRepository implements ObjectInterface
 {
     /**
      * Holds instance of Core/DatabaseConnection
@@ -27,7 +27,7 @@ abstract class ObjectRepository implements ObjectInterface
         $params = array();
         $params['driver'] = "mysql";
         $params['host'] = "localhost";
-        $params['dbname'] = "OnlineShop";
+        $params['dbname'] = "onlineshop";
         $params['username'] = "root";
         $params['password'] = "";
         
@@ -51,27 +51,28 @@ abstract class ObjectRepository implements ObjectInterface
         $entityKeyValues = array_values($entity->getAttributes());
 
         // Database inser prepare 
-        echo "INSERT INTO " . "'" . $entity->tableName . "'" . " (" . implode("," , $entityKeyNames) ."') VALUES (" . implode("," , $entityKeyValues) . ")";
-        $query = $this->db->DatabaseConnection->exec("INSERT INTO " . "'" . $entity->tableName . "'" . " (" . implode("," , $entityKeyNames) ."') VALUES (" . implode("," , $entityKeyValues) . ")"); 
-
+        $query = $this->db->DatabaseConnection->exec('INSERT INTO ' . $entity->tableName . " VALUES ('id','" . implode("','" , $entityKeyValues) . "')");
+        if(!$query)
+        {
+            var_dump($this->db->DatabaseConnection->errorInfo());
+        }
         // Clears query
         $query = "";
     }
 
     /**
-     * function read()
+     * function findById()
      * 
      * holds logic for reading new instance of Core\BaseObject\BaseObject
      * 
-     * @param $id id of Core\BaseObject\BaseObject
+     * @param $attributes assosiative array [ attributes[{ENTITY} tableName] , attributes[{ENTITY} primaryKeyName] ] 
      */
-    public function read($entity)
+    public function findById($attributes)
     {
-        // Database inser prepare
-        echo "SELECT * FROM " . $entity->tableName . "WHERE ID = " . $entity->id;
-        $query = $this->db->DatabaseConnection->execute("SELECT * FROM " . $entity->tableName . "WHERE ID = " . $entity->id); 
-        
-        // Fetches results from query
+        // Database prepare insert 
+        $query = $this->db->DatabaseConnection->query("SELECT * FROM ". $attributes['tableName'] . " WHERE id = " . $attributes['primaryKeyName']); 
+
+        //Fetches results from query
         $result = $query->fetch();
 
         // Returs array of results
@@ -88,7 +89,20 @@ abstract class ObjectRepository implements ObjectInterface
      * 
      * @param $user Core\BaseObject\BaseObject
      */
-    abstract public function update($user);
+    public function update($attributes)
+    {
+        // $entityKeyNames holds entity attrbute names  
+        $attributesKeyNames = array_keys($attributes->getAttributes());
+
+        // $entityKeyValues holds entity attrbute values 
+        $attributesKeyValues = array_values($attributes->getAttributes());
+
+        // Database prepare update
+        // No prepare way?
+        $query = $this->db->DatabaseConnection->prepare("UPDATE " . "'" . $attributes->tableName . "'" . "SET(" . implode("=?," , $attributesKeyNames) ."=?') WHERE id = " . $attributes->id);
+        $query = $this->db->DatabaseConnection->exec([$attributesKeyValues]); 
+   
+    }
 
     /**
      * function delete()
@@ -97,5 +111,8 @@ abstract class ObjectRepository implements ObjectInterface
      * 
      * @param $user Core\BaseObject\BaseObject
      */
-    abstract public function delete($user);
+    public function delete($user)
+    {
+
+    }
 }
